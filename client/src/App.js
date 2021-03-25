d4b0da870ab33dea9a21854a20cb6d5bebb8c5cd
Stephan Iskander <stephan.iskander@gmail.com>
2021-03-25 19:32:03 -0400

    File Initialization

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Table from "./components/Table";
 
 const App = () => {
     return(
         <div>
             <h1>timeSlot App</h1>
+            <Table />
         </div>
     );
 }
