04178d83563541fe9f6f9b03a4d80f2a0fdd46d0
Stephan Iskander <stephan.iskander@gmail.com>
2021-03-26 08:16:34 -0400

    Server side files add in

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import myFunction from "./apiCall.js"
 
 const app = express();
 
 app.use(cors());
 
+// var myFunctionTest = require("./apiCall.js");
+
+myFunctionTest();
